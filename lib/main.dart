e2a1ef0d750a607c08a936130e279c0a0a79e3db
Jermaine Oppong <jeropp00@gmail.com>
2020-08-28 14:38:53 +0100

    Restyled search results. Add search filters route

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 void main() async {
   await DotEnv().load('.env');
@@ -41,32 +42,53 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
-  List _restaurants;
+  String query;
 
-  void searchRestaurants(String query) async {
+  Future<List> searchRestaurants(String query) async {
     final response = await widget.dio.get('', queryParameters: {
       'q': query,
     });
-    setState(() {
-      _restaurants = response.data['restaurants'];
-    });
+    return response.data['restaurants'];
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(
+          widget.title,
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
         backgroundColor: Colors.red,
+        actions: [
+          InkWell(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => SearchFilters()),
+              );
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Icon(
+                Icons.tune,
+              ),
+            ),
+          ),
+        ],
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             SearchForm(
-              onSearch: searchRestaurants,
+              onSearch: (q) {
+                setState(() {
+                  query = q;
+                });
+              },
             ),
-            _restaurants == null
+            query == null
                 ? Expanded(
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
@@ -87,25 +109,150 @@ class _SearchPageState extends State<SearchPage> {
                       ],
                     ),
                   )
-                : Expanded(
-                    child: ListView(
-                      children: _restaurants.map((restaurant) {
-                        return ListTile(
-                          title: Text(restaurant['restaurant']['name']),
-                          subtitle: Text(
-                              restaurant['restaurant']['location']['address']),
-                          trailing: Text(
-                              '${restaurant['restaurant']['user_rating']['aggregate_rating']} stars, '
-                              '${restaurant['restaurant']['all_reviews_count']} reviews'),
+                : FutureBuilder(
+                    future: searchRestaurants(query),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(
+                          child: CircularProgressIndicator(),
                         );
-                      }).toList(),
-                    ),
-                  ),
+                      }
+
+                      if (snapshot.hasData) {
+                        return Expanded(
+                          child: ListView(
+                            children: snapshot.data
+                                .map<Widget>(
+                                    (json) => RestaurantItem(Restaurant(json)))
+                                .toList(),
+                          ),
+                        );
+                      }
+
+                      return Text(
+                          'Error retrieving results: ${snapshot.error}');
+                    },
+                  )
           ],
         ),
       ),
     );
   }
+}
+
+class RestaurantItem extends StatelessWidget {
+  final Restaurant restaurant;
+
+  RestaurantItem(this.restaurant);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 6),
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: Row(
+          children: [
+            restaurant.thumbnail != null && restaurant.thumbnail.isNotEmpty
+                ? Ink(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.blueGrey,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(restaurant.thumbnail),
+                      ),
+                    ),
+                  )
+                : Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.blueGrey,
+                    child: Icon(
+                      Icons.restaurant,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                  ),
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      restaurant.name,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 7),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.location_on,
+                          color: Colors.redAccent,
+                          size: 15,
+                        ),
+                        SizedBox(width: 5),
+                        Text(restaurant.locality),
+                      ],
+                    ),
+                    SizedBox(height: 5),
+                    RatingBarIndicator(
+                      rating: double.parse(restaurant.rating),
+                      itemBuilder: (_, __) {
+                        return Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        );
+                      },
+                      itemSize: 20,
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Restaurant {
+  final String id;
+  final String name;
+  final String address;
+  final String locality;
+  final String rating;
+  final int reviews;
+  final String thumbnail;
+
+  Restaurant._({
+    this.id,
+    this.name,
+    this.address,
+    this.locality,
+    this.rating,
+    this.reviews,
+    this.thumbnail,
+  });
+  factory Restaurant(Map json) => Restaurant._(
+      id: json['restaurant']['id'],
+      name: json['restaurant']['name'],
+      address: json['restaurant']['location']['address'],
+      locality: json['restaurant']['location']['locality'],
+      rating: json['restaurant']['user_rating']['aggregate_rating']?.toString(),
+      reviews: json['restaurant']['all_reviews_count'],
+      thumbnail:
+          json['restaurant']['featured_image'] ?? json['restaurant']['thumb']);
 }
 
 class SearchForm extends StatefulWidget {
@@ -158,6 +305,8 @@ class _SearchFormState extends State<SearchForm> {
                   final isValid = _formKey.currentState.validate();
                   if (isValid) {
                     widget.onSearch(_search);
+                    // Collapses keypad
+                    FocusManager.instance.primaryFocus.unfocus();
                   } else {
                     setState(() {
                       _autoValidate = true;
@@ -183,6 +332,25 @@ class _SearchFormState extends State<SearchForm> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class SearchFilters extends StatefulWidget {
+  @override
+  _SearchFiltersState createState() => _SearchFiltersState();
+}
+
+class _SearchFiltersState extends State<SearchFilters> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Filter your search'),
+        backgroundColor: Colors.red,
+      ),
+      backgroundColor: Colors.red,
+      body: Text('Filter your search'),
     );
   }
 }
